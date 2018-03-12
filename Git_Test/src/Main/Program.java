f67a4fceb5c20c436d7bf8a5284db7d48e70a14f
Adrian <Adrian@DESKTOP-A9LS65Q>
2018-03-12 23:54:26 +0200

    Functionality addeed

--- Git_Test/src/Main/Program.java
+++ Git_Test/src/Main/Program.java
@@ -5,6 +5,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		System.out.println("First git Project - version: 2.0");
+		
+		System.out.println("Added some new, still undocumented functionalioty");
 
 	}
 
